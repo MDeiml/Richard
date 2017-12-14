61f28280bc798a987720c58dc5378e522e89bf19
MDeiml <matthias@deiml.net>
2017-12-14 15:39:05 +0100

    Better cards and delted (right this time) resource

--- app/src/main/java/com/mdeiml/richard/Card.java
+++ app/src/main/java/com/mdeiml/richard/Card.java
@@ -3,20 +3,21 @@ import android.widget.FrameLayout;
 import android.widget.Checkable;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.support.v7.widget.CardView;
 
-public class Card extends FrameLayout implements Checkable {
+public class Card extends CardView implements Checkable {
     
     boolean checked;
     
     public Card(Context c, AttributeSet attr) {
         super(c, attr);
-        setBackgroundResource(R.drawable.card_background);
+        setCardBackgroundColor(R.drawable.card_background);
         checked = false;
     }
     
     @Override
     public void setChecked(boolean p1) {
-        setBackgroundResource(p1 ? R.drawable.card_background_selected : R.drawable.card_background);
+        setCardBackgroundColor(getContext().getColor(p1 ? R.color.card_background_selected : R.color.card_background));
         checked = p1;
     }
 
